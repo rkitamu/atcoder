a0a00e2ca9b9ccedb811a75da593558a210fdcaf
rkitamu <rkitamun@gmail.com>
2025-05-18 09:54:20 +0000

    feat: enhance Go problem template with buffered I/O and helper functions

--- problems/template/x/main.go
+++ problems/template/x/main.go
@@ -1,9 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
 
 func main() {
-	// Read two integers from a single line of standard input
-	var x, y int
-	fmt.Scan(&x, &y)
+	defer flush()
+	n := ni() // input single int
+	_ = nis(n) // input n ints 
+
+	out(n) // output result
+}
+
+// =====================
+// utils
+// =====================
+// io
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+
+// =====================
+// input utils
+// =====================
+// ni reads a single integer from stdin.
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// ni reads n integers from stdin.
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+
+// =====================
+// output utils
+// =====================
+// flush flushes the buffered writer.
+func flush() {
+	e := wtr.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
+
+// out writes the output to stdout.
+func out(v ...interface{}) {
+	_, e := fmt.Fprintln(wtr, v...)
+	if e != nil {
+		panic(e)
+	}
 }
